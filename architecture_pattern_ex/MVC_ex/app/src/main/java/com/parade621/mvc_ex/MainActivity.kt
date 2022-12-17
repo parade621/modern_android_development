5e336b99580070e715eee54bf2f768d6d6209088
parade621 <parade621@naver.com>
2022-12-17 21:00:15 +0900

    mvc pattern
    간단한 로그인 어플리케이션을 MVC 패턴으로 구현한 것입니다.

--- architecture_pattern_ex/MVC_ex/app/src/main/java/com/parade621/mvc_ex/MainActivity.kt
+++ architecture_pattern_ex/MVC_ex/app/src/main/java/com/parade621/mvc_ex/MainActivity.kt
@@ -1,11 +1,24 @@
  package com.parade621.mvc_ex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.parade621.mvc_ex.databinding.ActivityMainBinding
+import com.parade621.mvc_ex.mvc.controller.MvcLoginActivity
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+
+    private val binding : ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.mvcBtn.setOnClickListener {
+            val intent = Intent(this, MvcLoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

8efd23a631f9a9ee57af10ab92fe7899dea14dcf
parade621 <parade621@naver.com>
2022-12-19 14:02:32 +0900

    프로젝트 재생성

---
+++ architecture_pattern_ex/MVVM_ex/app/src/main/java/com/parade621/mvvm_ex/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.parade621.mvvm_ex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

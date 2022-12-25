61a96e40b0fbea72ac8a0ba6776e1b21ceab3012
parade621 <parade621@naver.com>
2022-12-26 01:39:50 +0900

    Repository Pattern Basic

---
+++ DataBindingBasic/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+v
+Fapp/src/main/java/com/parade621/databindingbasic/MyViewModelFactory.kt,d\5\d5525dac678703b3bda99a4f55439ddbd8fdb914
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+p
+@app/src/main/java/com/parade621/databindingbasic/MainActivity.kt,2\4\24daa6fd16b61ab71ff161bef3b889c8dd28f303
+s
+Capp/src/main/java/com/parade621/databindingbasic/BindingAdapters.kt,9\e\9e496004bfaecf48a0989948831db7d1d5d08d97
+o
+?app/src/main/java/com/parade621/databindingbasic/MyViewModel.kt,5\4\544f3845fd0d46a8e70bcc05f9ee549cedd8776a
---
+++ RepositoryPatternBasic/app/src/main/java/com/parade621/repositoryPatternBasic/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.parade621.repositoryPatternBasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.parade621.repositoryPatternBasic.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val factory = MyViewModelFactory(10, this)
+        val myViewModel : MyViewModel by viewModels { factory }
+
+        binding.lifecycleOwner= this
+        binding.viewmodel = myViewModel
+
+        binding.textView.text = myViewModel.counter.toString()
+
+        binding.btn.setOnClickListener {
+            myViewModel.liveCounter.value = myViewModel.liveCounter.value?.plus(1)
+        }
+    }
+}
